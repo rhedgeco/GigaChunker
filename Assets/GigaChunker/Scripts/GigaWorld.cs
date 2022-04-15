4f731af79c51361315b4bb07c17d417613f8d364
rhedgeco <rhedgeco@gmail.com>
2022-04-14 23:19:10 -0700

    Added giga chunk data movement

--- Assets/GigaChunker/Scripts/GigaWorld.cs
+++ Assets/GigaChunker/Scripts/GigaWorld.cs
@@ -1,9 +1,42 @@
+using GigaChunker.Jobs;
+using Unity.Jobs;
 using UnityEngine;
 
 namespace GigaChunker
 {
     public class GigaWorld : MonoBehaviour
     {
-        
+        [SerializeField, Range(8, 64)] private int chunkSize = 32;
+        [SerializeField, Range(1, 16)] private int renderDistance = 4;
+        [SerializeField] private bool debug;
+
+        private GigaChunkData.ChunkDataManager _dataManager;
+
+        private void Awake()
+        {
+            _dataManager = new(chunkSize, renderDistance);
+        }
+
+        private void Update()
+        {
+            _dataManager.SetPlayerPosition(transform.position);
+            _dataManager.Run();
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!debug || !Application.isPlaying) return;
+            Gizmos.color = Color.red;
+            _dataManager.ForEach((ref GigaChunkData data, int _) =>
+            {
+                if (!GigaChunkData.RefInRange(ref data)) return;
+                Gizmos.DrawWireCube(GigaChunkData.RefWorldCenter(ref data), Vector3.one * data.ChunkSize);
+            });
+        }
+
+        private void OnDestroy()
+        {
+            _dataManager.Dispose();
+        }
     }
 }
---
+++ Assets/GigaChunker/Scripts/Jobs/GigaChunkData.cs
@@ -0,0 +1,47 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace GigaChunker.Jobs
+{
+    public partial struct GigaChunkData
+    {
+        public readonly int ChunkSize;
+        public readonly int ChunkIndex;
+        public readonly int3 Chunk3dIndex;
+
+        private int3 _chunkPosition;
+        private float3 _worldPosition;
+        private float3 _worldCenter;
+        private Matrix4x4 _worldMatrix;
+        private bool _inRange;
+
+        // expose private editable values by ref so that copying does not occur on read
+        public static ref readonly int3 RefChunkPosition(ref GigaChunkData gigaChunkData) => ref gigaChunkData._chunkPosition;
+        public static ref readonly float3 RefWorldPosition(ref GigaChunkData gigaChunkData) => ref gigaChunkData._worldPosition;
+        public static ref readonly float3 RefWorldCenter(ref GigaChunkData gigaChunkData) => ref gigaChunkData._worldCenter;
+        public static ref readonly Matrix4x4 RefWorldMatrix(ref GigaChunkData gigaChunkData) => ref gigaChunkData._worldMatrix;
+        public static ref readonly bool RefInRange(ref GigaChunkData gigaChunkData) => ref gigaChunkData._inRange;
+
+        public GigaChunkData(int chunkSize, int index, int3 index3d)
+        {
+            ChunkSize = chunkSize;
+            ChunkIndex = index;
+            Chunk3dIndex = index3d;
+
+            _chunkPosition = int3.zero;
+            _worldPosition = float3.zero;
+            _worldCenter = _worldPosition + new float3(chunkSize / 2f);
+            _worldMatrix = Matrix4x4.Translate(_worldPosition);
+            _inRange = false;
+        }
+
+        private void MoveChunk(int3 chunkPosition)
+        {
+            if (_chunkPosition.Equals(chunkPosition)) return;
+            _chunkPosition = chunkPosition;
+            _worldPosition = chunkPosition * ChunkSize;
+            _worldCenter = _worldPosition + new float3(ChunkSize / 2f);
+            _worldMatrix = Matrix4x4.Translate(_worldPosition);
+        }
+    }
+}
