25a3e62bcc6f1238616fde1954a7a856dd8cc135
rhedgeco <rhedgeco@gmail.com>
2022-04-18 12:35:31 -0700

    removed node generation

--- Assets/GigaChunker/Scripts/Testers/MeshingSpeedLimitTester.cs
+++ Assets/GigaChunker/Scripts/Testers/MeshingSpeedLimitTester.cs
@@ -1,8 +1,3 @@
-using GigaChunker.DataTypes;
-using GigaChunker.Jobs;
-using Unity.Collections;
-using Unity.Jobs;
-using Unity.Mathematics;
 using UnityEngine;
 
 namespace GigaChunker.Testers
@@ -11,22 +6,5 @@ namespace GigaChunker.Testers
     {
         [SerializeField, Range(8, 64)] private int chunkSize = 32;
 
-        private NativeArray3d<GigaNode> _nodes;
-
-        private void Start()
-        {
-            _nodes = new(chunkSize, Allocator.Persistent);
-            GenerateNodesJob job = new()
-            {
-                Position = int3.zero,
-                Nodes = _nodes
-            };
-            job.Run();
-        }
-
-        private void OnDestroy()
-        {
-            if (_nodes.IsCreated) _nodes.Dispose();
-        }
     }
 }
